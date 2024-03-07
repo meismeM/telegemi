6456d1cfcca963dca3c7056b28a410b303a9ffd5
meismeM <97947730+meismeM@users.noreply.github.com>
2024-03-07 15:14:04 +0300

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,4 +1,4 @@
-﻿import os
+import os
 from re import split
 
 """ Required """
@@ -10,11 +10,11 @@ GOOGLE_API_KEY = split(r'[ ,;，；]+', os.getenv("GOOGLE_API_KEY", ''))
 
 ALLOWED_USERS = split(r'[ ,;，；]+', os.getenv("ALLOWED_USERS", '').replace("@", "").lower())
 #Whether to push logs and enable some admin commands
-IS_DEBUG_MODE = os.getenv("IS_DEBUG_MODE", '1')
+IS_DEBUG_MODE = os.getenv("IS_DEBUG_MODE", '0')
 #The target account that can execute administrator instructions and log push can use /get_my_info to obtain the ID.
 ADMIN_ID = os.getenv("ADMIN_ID", "1470186445")
 #Determines whether to verify identity. If 0, anyone can use the bot. It is enabled by default.
-AUCH_ENABLE = os.getenv("AUCH_ENABLE", "0")
+AUCH_ENABLE = os.getenv("AUCH_ENABLE", "1")
 
 """ read https://ai.google.dev/api/rest/v1/GenerationConfig """
 generation_config = {
