8efb16d1f9c2a14e0b90ee5b5998a45ca10d8766
meismeM <97947730+meismeM@users.noreply.github.com>
2025-03-20 12:21:36 +0300

    Update config.py

--- api/config.py
+++ api/config.py
@@ -21,7 +21,7 @@ AUCH_ENABLE = os.getenv("AUCH_ENABLE", "0")
 
 """ read https://ai.google.dev/api/rest/v1/GenerationConfig """
 generation_config = {
-    "max_output_tokens": 1024,
+    "max_output_tokens": 64k,
 }
 
 """ read https://ai.google.dev/api/rest/v1/HarmCategory """
